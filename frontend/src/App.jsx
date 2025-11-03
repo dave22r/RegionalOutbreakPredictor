856015d30ca75c3ab9299e9ad5454c9a403cb97e
ChakornK <chakornk2007@gmail.com>
2025-11-03 15:43:12 -0800

    feat(frontend): add Google Maps rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,68 @@
+import { DeckGL } from "@deck.gl/react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
 import "./App.css";
+import { useEffect, useState } from "react";
+
+const { VITE_GOOGLE_MAPS_API_KEY, VITE_GOOGLE_MAPS_ID } = import.meta.env;
+
+const LOADING_TIME_LIMIT = 3000;
+const DEFAULT_ZOOM = 12;
 
 function App() {
+  const [ready, setReady] = useState(false);
+  const [viewState, setViewState] = useState({
+    longitude: -122.4,
+    latitude: 37.8,
+    zoom: DEFAULT_ZOOM,
+  });
+  useEffect(() => {
+    let loadTimedout = false;
+    const timer = setTimeout(() => {
+      loadTimedout = true;
+      setReady(true);
+    }, LOADING_TIME_LIMIT);
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        if (!loadTimedout) {
+          clearTimeout(timer);
+          setViewState({
+            ...viewState,
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+            zoom: DEFAULT_ZOOM,
+          });
+          setReady(true);
+        }
+      },
+      () => setReady(true)
+    );
+  }, []);
+
   return (
-    <main>
-      <p>Regional Outbreak Predictor</p>
+    <main className="w-screen h-dscreen">
+      {ready ? (
+        <APIProvider apiKey={VITE_GOOGLE_MAPS_API_KEY}>
+          <DeckGL initialViewState={viewState} controller>
+            <Map
+              mapId={VITE_GOOGLE_MAPS_ID}
+              renderingType="VECTOR"
+              colorScheme="DARK"
+            ></Map>
+          </DeckGL>
+        </APIProvider>
+      ) : (
+        <LoadingScreen />
+      )}
     </main>
+  );
+}
+
+function LoadingScreen() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="font-bold text-xl text-center">Outbreak Predictor</p>
+    </div>
   );
 }
 
