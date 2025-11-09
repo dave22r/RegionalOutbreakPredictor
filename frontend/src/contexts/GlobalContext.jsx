dc8af614383388905a8964b7711cf399d981b0a2
ChakornK <120770627+ChakornK@users.noreply.github.com>
2025-11-09 08:11:32 -0800

    fix: initialize userData state as empty

--- frontend/src/contexts/GlobalContext.jsx
+++ frontend/src/contexts/GlobalContext.jsx
@@ -3,11 +3,7 @@ import { createContext, useState } from "react";
 export const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
-  const [userData, setUserData] = useState({
-    name: "",
-    email: "",
-    photoURL: "",
-  });
+  const [userData, setUserData] = useState({});
 
   return (
     <GlobalContext.Provider value={{ userData, setUserData }}>
