6880d377fe9fe798858b8e4ab5fe51a124cdb816
Ryan Huang <ryanhuang6391@gmail.com>
2025-11-30 00:01:28 -0800

    feat(ml): trained initial model

---
+++ ml/src/model.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    classification_report,
+    accuracy_score,
+    roc_auc_score,
+    confusion_matrix,
+)
+import joblib
+
+# run from project root
+DF_PATH = Path("ml/data/processed/test_dataset.csv")
+MODEL_PATH = Path("ml/models/logistic_model.pkl")
+SCALER_PATH = Path("ml/models/scaler.pkl")
+
+df = pd.read_csv(DF_PATH)
+
+y = df["FUTURE_OUTBREAK"]
+X = df.drop("FUTURE_OUTBREAK", axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=67
+)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model = LogisticRegression(max_iter=1000, random_state=67, solver="lbfgs")
+model.fit(X_train_scaled, y_train)
+
+y_train_pred = model.predict(X_train_scaled)
+y_train_pred_proba = model.predict_proba(X_train_scaled)[:, 1]
+
+y_test_pred = model.predict(X_test_scaled)
+y_test_pred_proba = model.predict_proba(X_test_scaled)[:, 1]
+
+print("\n" + "=" * 70)
+print("MODEL PERFORMANCE - TRAINING")
+print("=" * 70)
+print(f"Accuracy: {accuracy_score(y_train, y_train_pred):.4f}")
+print(f"ROC-AUC: {roc_auc_score(y_train, y_train_pred_proba):.4f}")
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_train, y_train_pred))
+print("\nClassification Report:")
+print(
+    classification_report(
+        y_train, y_train_pred, target_names=["No Outbreak", "Outbreak"]
+    )
+)
+
+print("\n" + "=" * 70)
+print("MODEL PERFORMANCE - TESTING")
+print("=" * 70)
+print(f"Accuracy: {accuracy_score(y_test, y_test_pred):.4f}")
+print(f"ROC-AUC: {roc_auc_score(y_test, y_test_pred_proba):.4f}")
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_test_pred))
+print("\nClassification Report:")
+print(
+    classification_report(y_test, y_test_pred, target_names=["No Outbreak", "Outbreak"])
+)
+
+
+joblib.dump(model, MODEL_PATH)
+joblib.dump(scaler, SCALER_PATH)
