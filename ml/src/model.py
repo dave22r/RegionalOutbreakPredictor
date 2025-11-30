ad1845eeab3cea7f23465d75ed523699165f636a
Ryan Huang <ryanhuang6391@gmail.com>
2025-11-30 00:02:41 -0800

    chore(ml): clean up imports

--- ml/src/model.py
+++ ml/src/model.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
